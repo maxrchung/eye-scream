755612575c86767829e81e1bb70b03b9d14dd2bb
Julia Ahopelto <me@notjulie.dev>
2025-08-30 14:40:38 -0700

    make the camera good yeah

--- EyeScream/Assets/Scripts/Controller/CameraController.cs
+++ EyeScream/Assets/Scripts/Controller/CameraController.cs
@@ -1,30 +1,49 @@
-using System;
+using Config;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
-public class CameraController : MonoBehaviour
+namespace Controller
 {
-    public float sensitivity = 2.0f;
-
-    private float _pitch = 0.0f;
-    private float _yaw = 0.0f;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class CameraController : MonoBehaviour
     {
-        _pitch = transform.eulerAngles.x;
-        _yaw = transform.eulerAngles.y;
-    }
+        public float mouseSensitivity = 80f;
+        public float gamepadSensitivity = 50f;
 
-    private void Update()
-    {
-        if (Mouse.current.leftButton.isPressed)
+        private float _pitch = 0.0f;
+        private float _yaw = 0.0f;
+        private InputActions _inputActions;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
         {
-            var delta = Mouse.current.delta.ReadValue();
-            _yaw -= sensitivity * delta.x;
-            _pitch += sensitivity * delta.y;
+            _pitch = transform.eulerAngles.x;
+            _yaw = transform.eulerAngles.y;
+            _inputActions = new InputActions();
+            _inputActions.Player.Enable();
+        }
+
+        private void RotateCamera(Vector2 delta)
+        {
+            _pitch -= delta.y;
+            _yaw += delta.x;
+            _pitch = Mathf.Clamp(_pitch, -89f, 89f);
             transform.eulerAngles = new Vector3(_pitch, _yaw, 0.0f);
+        }
+
+        private void Update()
+        {
+            if (_inputActions.Player.LookIndirectTrigger.IsPressed())
+            {
+                var updateDelta = _inputActions.Player.LookIndirect.ReadValue<Vector2>() *
+                                  (0.01f * mouseSensitivity * -1f);
+                RotateCamera(updateDelta);
+            }
+            else if (_inputActions.Player.LookDirect.IsInProgress())
+            {
+                var updateDelta = _inputActions.Player.LookDirect.ReadValue<Vector2>() *
+                                  (Time.deltaTime * gamepadSensitivity);
+                RotateCamera(updateDelta);
+            }
         }
     }
 }
